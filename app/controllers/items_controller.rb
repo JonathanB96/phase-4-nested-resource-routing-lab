103acd710985cd532efa3645edda7321331c602f
jonathan bola <bolajonathan@yahoo.com>
2022-07-05 18:19:42 -0500

    commit tree 8d89fc3464a1e365c2bf8919a8cc0a4bb53ea0f6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,46 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from NoMethodError, with: :render_not_found_response
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+
+      user = User.find_by(id: params[:user_id])
+      all_items = user.items
+      render json: all_items, include: :user
+    else
+      items = Item.all 
+      render json: items, include: :user
+    end
+
+    
   end
+
+  def show
+    if params[:user_id] && params[:id]
+      item = Item.find(params[:id])
+      render json: item
+    else
+
+     
+    end
+  
+
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      
+      item = user.items.create(params.permit(:name, :description, :price))
+      render json: item, status: :created
+    end
+
+
+  end
+  private
+  def render_not_found_response
+    render json: { error: "Not found" }, status: :not_found
+  end
+
 
 end
